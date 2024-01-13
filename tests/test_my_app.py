cfb9e890591f401f0c33e2df0518c1f978232638
arnaneelsen <arnaneelsen@gmail.com>
2024-01-13 19:01:40 +0530

    third push

--- tests/test_my_app.py
+++ tests/test_my_app.py
@@ -1,17 +1,9 @@
 import unittest
-
-  from my_app import add
- 
+  from my_app import add 
   class TestMyApp(unittest.TestCase):
-
       def test_add(self):
-
-        self.assertEqual(add(1,2),3)
- 
+        self.assertEqual(add(1,2),3) 
       def test_add_negative_numbers(self):
-
         self.assertEqual(add(-1,-2),-3)
-
   if __name__ == '__main__':
-
       unittest.main()

816d2a2d00110a7bee00f3619f208586d6c0a20d
arnaneelsen <arnaneelsen@gmail.com>
2024-01-15 19:15:56 +0530

    last try lol

--- tests/test_my_app.py
+++ tests/test_my_app.py
@@ -1,9 +1,9 @@
 import unittest
-  from my_app import add 
-  class TestMyApp(unittest.TestCase):
-      def test_add(self):
-        self.assertEqual(add(1,2),3) 
-      def test_add_negative_numbers(self):
-        self.assertEqual(add(-1,-2),-3)
-  if __name__ == '__main__':
-      unittest.main()
+from my_app import add
+class TestMyApp(unittest.TestCase):
+	def test_add(self):
+            self.assertEqual(add(1,2),3) 
+        def test_add_negative_numbers(self):
+            self.assertEqual(add(-1,-2),-3)
+if __name__ == '__main__':
+	unittest.main()
